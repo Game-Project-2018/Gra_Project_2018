2ffe10b0fd0823f2d0d3e688313ac892717dc53a
Matemburg <VonMatemburg@gmail.com>
2018-11-10 23:24:54 +0100

    Inventory Test

--- Assets/FieldOfView/ViewRotationController.cs
+++ Assets/FieldOfView/ViewRotationController.cs
@@ -5,6 +5,9 @@ using UnityEditor;
 
 public class ViewRotationController : MonoBehaviour
 {
+	GameObject inventory;
+	GameObject craftSystem;
+	GameObject characterSystem;
 
     [Header("Freeze Rotation")]
     public bool x;
@@ -14,9 +17,22 @@ public class ViewRotationController : MonoBehaviour
     private float xRotation;
     private float yRotation;
     private float zRotation;
+	public PlayerInventory playerInv;
 
     void Start()
     {
+
+		if (GameObject.FindGameObjectWithTag("Player") != null)
+		{
+
+			if (playerInv.inventory != null)
+				inventory = playerInv.inventory;
+			if (playerInv.craftSystem != null)
+				craftSystem = playerInv.craftSystem;
+			if (playerInv.characterSystem != null)
+				characterSystem = playerInv.characterSystem;
+		}
+
         xRotation = transform.eulerAngles.x;
         yRotation = transform.eulerAngles.x;
         zRotation = transform.eulerAngles.x;
@@ -24,16 +40,29 @@ public class ViewRotationController : MonoBehaviour
 
     void Update()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Plane plane = new Plane(Vector3.up, transform.position);
-        float distance = 0;
-        if (plane.Raycast(ray, out distance))
-        {
-            Vector3 mousePos = ray.GetPoint(distance);
-            transform.LookAt(mousePos + Vector3.up * transform.position.y);
-        }
-        FreezeRotation();
+		if (!lockMovement()) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			Plane plane = new Plane (Vector3.up, transform.position);
+			float distance = 0;
+			if (plane.Raycast (ray, out distance)) {
+				Vector3 mousePos = ray.GetPoint (distance);
+				transform.LookAt (mousePos + Vector3.up * transform.position.y);
+			}
+			FreezeRotation ();
+		}
     }
+
+	bool lockMovement()
+	{
+		if (inventory != null && inventory.activeSelf)
+			return true;
+		else if (characterSystem != null && characterSystem.activeSelf)
+			return true;
+		else if (craftSystem != null && craftSystem.activeSelf)
+			return true;
+		else
+			return false;
+	}
 
     private void FreezeRotation()
     {
