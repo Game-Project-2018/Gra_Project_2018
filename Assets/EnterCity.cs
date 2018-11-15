eba2f2cc1066f48a4f0bdea46e137a7954b1bbd5
Damian Karwowski <damiankarwowski9@gmail.com>
2018-11-15 23:22:37 +0100

    Camera zoom, areas that affect player movement, city with button enter city

---
+++ Assets/EnterCity.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterCity : MonoBehaviour {
+
+    public Canvas GetCanvas;
+
+    void OnTriggerEnter(Collider Player)
+    {
+        if (Player.tag == "Player")
+        {
+            Player.GetComponent<PlayerMovementWorldMap>().speed = 1;
+            GetCanvas.enabled = true;
+        }
+    }
+    void OnTriggerExit(Collider Player)
+    {
+        if (Player.tag == "Player")
+        {
+            Player.GetComponent<PlayerMovementWorldMap>().speed = 4;
+            GetCanvas.enabled = false;
+        }
+    }
+}
